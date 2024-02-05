1121cc9d69d7bea9906ac2fd41735c5ea92282f3
Pavel Novik <brn2befree@gmail.com>
2024-02-05 18:33:52 +0300

    add comments for handle server app error function

--- src/common/utils/handle-server-app-error.ts
+++ src/common/utils/handle-server-app-error.ts
@@ -2,6 +2,13 @@ import { Dispatch } from "redux";
 import { appActions } from "app/app.reducer";
 import { BaseResponseType } from "common/types/common.types";
 
+/**
+ * This function show error when it occurred in local
+ * @param data - The response data from the server.
+ * @param dispatch - The Redux dispatch function.
+ * @param showError - Whether to display an error message (default: true).
+ * @returns - nothing
+ */
 export const handleServerAppError = <D>(data: BaseResponseType<D>, dispatch: Dispatch, showError: boolean = true) => {
   if (showError) {
     if (data.messages.length) {
