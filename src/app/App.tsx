9ef1b2ff9f30c0568efe7416b1b45051954c7da0
Pavel Novik <brn2befree@gmail.com>
2024-02-05 19:59:34 +0300

    add hook useActions and use it in App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -17,7 +17,7 @@ import { authThunks } from "features/auth/auth.reducer";
 import "./App.css";
 import { TodolistsList } from "features/TodolistsList/TodolistsList";
 import { ErrorSnackbar } from "common/components";
-import { useAppDispatch } from "common/hooks";
+import { useActions, useAppDispatch } from "common/hooks";
 import { selectIsLoggedIn } from "features/auth/auth.selectors";
 import { selectAppStatus, selectIsInitialized } from "app/app.selectors";
 
@@ -26,14 +26,18 @@ function App() {
   const isInitialized = useSelector(selectIsInitialized);
   const isLoggedIn = useSelector(selectIsLoggedIn);
 
-  const dispatch = useAppDispatch();
+  // const dispatch = useAppDispatch();
+
+  const {initializeApp, logout} = useActions(authThunks)
 
   useEffect(() => {
-    dispatch(authThunks.initializeApp());
+    // dispatch(authThunks.initializeApp());
+    initializeApp()
   }, []);
 
   const logoutHandler = useCallback(() => {
-    dispatch(authThunks.logout());
+    // dispatch(authThunks.logout());
+    logout()
   }, []);
 
   if (!isInitialized) {
--- src/common/hooks/useActions.ts
+++ src/common/hooks/useActions.ts
@@ -2,6 +2,8 @@ import { useMemo } from "react";
 import { ActionCreatorsMapObject, bindActionCreators } from "redux";
 import { useAppDispatch } from "common/hooks/useAppDispatch";
 
+
+// variant 1
 export const useActions = <T extends ActionCreatorsMapObject>(actions: T) => {
   const dispatch = useAppDispatch();
 
@@ -20,3 +22,29 @@ type ReplaceReturnType<T, TNewReturn> = T extends (...args: any[]) => infer R
 type RemapActionCreators<T extends ActionCreatorsMapObject> = {
   [K in keyof T]: ReplaceReturnType<T[K], ActionCreatorResponse<T[K]>>;
 };
+
+
+
+// variant 2
+
+// import { ActionCreator, ActionCreatorsMapObject, AsyncThunk, bindActionCreators, } from '@reduxjs/toolkit';
+// import { useMemo } from 'react';
+// import { useAppDispatch } from 'common/hooks/useAppDispatch';
+//
+// export const useActions = <Actions extends ActionCreatorsMapObject = ActionCreatorsMapObject>
+// (actions: Actions): BoundActions<Actions> => {
+//   const dispatch = useAppDispatch();
+//
+//   return useMemo(() => bindActionCreators(actions, dispatch), []);
+// };
+//
+// // Types
+// type BoundActions<Actions extends ActionCreatorsMapObject> = {
+//   [key in keyof Actions]: Actions[key] extends AsyncThunk<any, any, any>
+//     ? BoundAsyncThunk<Actions[key]>
+//     : Actions[key];
+// };
+//
+// type BoundAsyncThunk<Action extends ActionCreator<any>> = (
+//   ...args: Parameters<Action>
+// ) => ReturnType<ReturnType<Action>>;
